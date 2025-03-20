a538d9ddb75249d3a4054e0d3090b0aa0779a8f4
06spacial <thaizaalmeida92@gmail.com>
2025-03-19 21:43:15 -0300

    Add files via upload

---
+++ aula1903.js
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////
+        /// Vetores/Arrays em JavaScript - Aula 9 ///
+
+let frutas = ["Maça", " Banana","Laranja"];
+ console.log(frutas);
+
+
+ frutas.push("Laranja");
+ console.log(frutas);
+
+ /// Atividades baseada na explicação é para montar em formato de lista
+
+ // 1 Crie uma função que recebe um array de números e retorna. A soma 
+ // a soma de todos os elementos
+
+ function somarElementos(array) {
+    let soma = 0;
+    let listaSomas = [];
+
+    for (let i = 0; i < array. length; i++) {
+        soma = listaSomar (push);
+        return lista
+    } /// elementos
+    numeros = 1,2,3,4,5,6,
+    //// mostre o resultado 
+
+    console.log = (somarElementos.length)
+ }
+
+
+
+
+
