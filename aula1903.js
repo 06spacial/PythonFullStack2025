5d43e44dde55d698e425c4c6a5557a9275341107
06spacial <thaizaalmeida92@gmail.com>
2025-03-24 20:47:12 -0300

    Add files via upload

--- aula1903.js
+++ aula1903.js
@@ -1,33 +1,47 @@
-//////////////////////////////////////////////////////////////////
-        /// Vetores/Arrays em JavaScript - Aula 9 ///
+// Aula que fala sobre vetores
+// Formação de lista
+// Uso do push para adicionar um ou mais elemento 
 
-let frutas = ["Maça", " Banana","Laranja"];
- console.log(frutas);
+//// Atividade1 ////
+// Crie uma função que receba o array de números e que retorne a soma de todos 
 
+function somaArray(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
 
- frutas.push("Laranja");
- console.log(frutas);
+let numeros1 = [1, 2, 3, 4, 5];
+console.log("Soma do array:", somaArray(numeros1));
 
- /// Atividades baseada na explicação é para montar em formato de lista
+//// Atividade2 ////
+// Crie uma função que receba um array de string e mostre o resultado em ordem alfabética
 
- // 1 Crie uma função que recebe um array de números e retorna. A soma 
- // a soma de todos os elementos
+function ordenarStrings(arr) {
+    return arr.sort();
+}
 
- function somarElementos(array) {
-    let soma = 0;
-    let listaSomas = [];
+let frutas2 = ["Banana", "Maçã", "Laranja", "Abacaxi"];
+console.log("Frutas ordenadas:", ordenarStrings(frutas2));
 
-    for (let i = 0; i < array. length; i++) {
-        soma = listaSomar (push);
-        return lista
-    } /// elementos
-    numeros = 1,2,3,4,5,6,
-    //// mostre o resultado 
+//// Atividade3 ////
+// Crie uma função que receba um array e que o retorne de forma duplicada 
 
-    console.log = (somarElementos.length)
- }
+function removerDuplicados(arr) {
+    return [...new Set(arr)];
+}
 
+let numeros3 = [1, 2, 2, 3, 4, 4, 5];
+console.log("Sem duplicados:", removerDuplicados(numeros3));
 
+// solução para atividade 3 usando Set e também uma solução usando Filter:
 
+function removerDuplicados(arr) {
+    return arr.filter((item, index) => arr.indexOf(item) === index);
+}
 
+let numeros4 = [1, 2, 2, 3, 4, 4, 5];
+console.log("Sem duplicados:", removerDuplicados(numeros4)); 
 
+////////////////////////////////////////////////////////
+// Observação //
+///// A atividade foi executada entorno do que foi passado em sala de aula. O assunto foi Vetores/Array - JavaScript
+// Usou-se como exeplos em aula a lista com frutas //
